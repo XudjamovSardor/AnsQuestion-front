0cc53bf4913c21074e5c1bc656d7153b68336645
Sardor <xudjamovsardor@gmail.com>
2022-04-19 14:55:59 +0300

    Add Router app-router.module.ts -- Add main menu (app.html, app.scss, app.ts) and Other

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnswerComponent } from './controller/answer/answer.component';
+import { PersonComponent } from './controller/person/person.component';
+import { QuestionComponent } from './controller/question/question.component';
+import { SubjectComponent } from './controller/subject/subject.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "user",
+    component: PersonComponent
+  },
+   {
+     path: "answer",
+     component: AnswerComponent
+   },
+   {
+     path: "question",
+     component: QuestionComponent
+   },
+   {
+     path: "subject",
+     component: SubjectComponent
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { material_import } from './material-import-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonComponent } from './controller/person/person.component';
+import { AnswerComponent } from './controller/answer/answer.component';
+import { QuestionComponent } from './controller/question/question.component';
+import { SubjectComponent } from './controller/subject/subject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    AnswerComponent,
+    QuestionComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...material_import,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
