6678af5cef2bf55d7a3d85899b6d7661343671a4
“Sardor” <xudjamovsardor12@gmail.com>
2022-05-12 10:07:21 +0500

    Add Error notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { PersonComponent } from './controller/person/person.component';
 import { AnswerComponent } from './controller/answer/answer.component';
 import { QuestionComponent } from './controller/question/question.component';
 import { SubjectComponent } from './controller/subject/subject.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ImgDialogComponent, QuestionDialogComponent, SubjectDialogComponent, UserInfoDialogComponent } from './shared/info-dialog.component';
 import { DashboardComponent } from './controller/dashboard/dashboard.component';
+import { ErorrInterceptor } from './core/error';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,11 @@ import { DashboardComponent } from './controller/dashboard/dashboard.component';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: ErorrInterceptor, multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
